b70ba374b4694205778b9dceaff2e4d5d04e44d9
yuxs <547205550@qq.com>
2021-02-19 16:29:44 +0800

    user模块提交

---
+++ yuxs-user-service/src/main/java/com/yuxs/user/service/YuxsUserServiceApplication.java
@@ -0,0 +1,17 @@
+package com.yuxs.user.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@EnableEurekaClient
+@SpringBootApplication
+public class YuxsUserServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YuxsUserServiceApplication.class, args);
+    }
+
+}
