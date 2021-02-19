b093be2e414b5c73c6b810ace1052a1e7baf6537
yuxs <547205550@qq.com>
2021-02-19 14:30:14 +0800

    test

---
+++ yuxs-eureka-server/src/main/java/com/yuxs/eureka/server/WebSecurityConfig.java
@@ -0,0 +1,24 @@
+package com.yuxs.eureka.server;
+
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    /**
+     * @param http
+     * @throws Exception
+     */
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http.csrf().disable();
+        super.configure(http);
+    }
+
+}
---
+++ yuxs-eureka-server/src/main/java/com/yuxs/eureka/server/YuxsEurekaServerApplication.java
@@ -0,0 +1,15 @@
+package com.yuxs.eureka.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class YuxsEurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YuxsEurekaServerApplication.class, args);
+    }
+
+}
